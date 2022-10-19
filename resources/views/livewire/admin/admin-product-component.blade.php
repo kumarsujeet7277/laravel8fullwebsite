c89c37c37316f15e5d094d9afef0e2c53ef1ee88
sujeet kumar <kumarsujeet5565@gmail.com>
2022-10-19 15:47:24 +0530

    AdminMakingSaleCarouselDynamic

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -32,7 +32,8 @@
                                     <th>Image</th>
                                     <th>Name</th>
                                     <th>Stock</th>
-                                    <th>Price</th>
+                                    <th>Regular Price</th>
+                                    <th>Sale Price</th>
                                     <th>Category</th>
                                     <th>Date</th>
                                     <th>Action</th>
@@ -46,6 +47,7 @@
                                         <td>{{$product->name}}</td>
                                         <td>{{$product->stock_status}}</td>
                                         <td>{{$product->regular_price}}</td>
+                                        <td>{{$product->sale_price}}</td>
                                         <td>{{$product->category->name}}</td>
                                         <td>{{$product->created_at}}</td>
                                         <td>
--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -1,5 +1,14 @@
 <!--main area-->
 <main id="main" class="main-site">
+    <style>
+        .regprice{
+            font-weight: 300;
+            font-size: 13px !important;
+            color: #aaaaaa !important;
+            text-decoration: line-through;
+            padding-left: 10px;
+        }
+    </style>
 
     <div class="container">
 
@@ -38,7 +47,13 @@
                         <div class="wrap-social">
                             <a class="link-socail" href="#"><img src="{{ asset('assets/images/social-list.png') }}" alt=""></a>
                         </div>
-                        <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
+                        @if ($product->sale_price > 0)
+                            <div class="wrap-price"><span class="product-price">${{$product->sale_price}}</span>
+                            <del><span class="product-price regprice">{{$product->regular_price}}</span></del></div>
+                        @else
+                            <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
+                        @endif
+                        
                         <div class="stock-info in-stock">
                             <p class="availability">Availability: <b>{{$product->stock_status}}</b></p>
                         </div>
