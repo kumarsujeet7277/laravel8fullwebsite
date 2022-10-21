c8edf888ddaf764b3b55f44afc9786d771a352eb
sujeet kumar <kumarsujeet5565@gmail.com>
2022-10-21 15:28:01 +0530

    AddProductToWishlist

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -9,27 +9,27 @@ class CartComponent extends Component
 {
     public function increaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty + 1;
-        Cart::update($rowId, $qty);
+        Cart::instance('cart')->update($rowId, $qty);
     }
 
     public function decreaseQuantity($rowId)
     {
-        $product = Cart::get($rowId);
+        $product = Cart::instance('cart')->get($rowId);
         $qty = $product->qty - 1;
-        Cart::update($rowId, $qty);
+        Cart::instance('cart')->update($rowId, $qty);
     }
  
     public function destroy($rowId)
     {
-        Cart::remove($rowId);
+        Cart::instance('cart')->remove($rowId);
         Session()->flash('success_message','Item has been removed');
     }
 
     public function destroyAll()
     {
-        Cart::destroy();
+        Cart::instance('cart')->destroy();
     }
 
     public function render()
--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -55,8 +55,31 @@
 
                 </div><!--end wrap shop control-->
 
+                <style>
+                    .product-wish{
+                        position: absolute;
+                        top: 10%;
+                        left: 0;
+                        z-index: 99;
+                        right: 30px;
+                        padding-top: 0;
+                        text-align: right;
+                    }
+                    .product-wish .fa{
+                        color: #cbcbcb;
+                        font-size: 32px;
+                    }
+                    .product-wish .fa:hover{
+                        color: #ff7007;
+                    }
+                    .fill-heart{
+                        color: #ff7007 !important;
+                    }
+                </style>
                 <div class="row">
-
+                    @php
+                        $wItems = Cart::instance('wishlist')->content()->pluck('id');
+                    @endphp
                     <ul class="product-list grid-products equal-container">
                         @foreach ($products as $product)
                         <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
@@ -70,6 +93,13 @@
                                     <a href="{{route('product.details',['slug'=>$product->slug])}}" class="product-name"><span>{{ $product->name }}</span></a>
                                     <div class="wrap-price"><span class="product-price">${{$product->regular_price}}</span></div>
                                     <a href="#" class="btn add-to-cart" wire:click.prevent="store({{$product->id}},'{{$product->name}}',{{$product->regular_price}})">Add To Cart</a>
+                                    <div class="product-wish">
+                                        @if ($wItems->contains($product->id))
+                                            <a href=""><i class="fa fa-heart fill-heart"></i></a>
+                                        @else
+                                            <a href="" wire:click.prevent="addToWishList({{$product->id}}, '{{$product->name}}',{{$product->regular_price}})"><i class="fa fa-heart"></i></a>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
                         </li>
@@ -231,6 +261,8 @@
 
 </main>
 <!--main area-->
+
+
 
 @push('scripts')
     <script>
