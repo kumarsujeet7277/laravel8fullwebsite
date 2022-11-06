eac7d5d73099471e55d56aa934798cecb66055e0
sujeet kumar <kumarsujeet5565@gmail.com>
2022-11-06 22:43:17 +0530

    ProblemOf ProductImageDeletationAndSolution

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -13,6 +13,18 @@ class AdminProductComponent extends Component
     public function deleteProduct($id)
     {
         $product = Product::find($id);
+        if($product->image)
+        {
+            unlink('assets/images/products'.'/'.$product->image);
+        }
+        if($product->images)
+        {
+            $images = explode(',',$product->images);
+            foreach($images as $image)
+            {
+                unlink('assets/images/products'.'/'.$image);
+            }
+        }
         $product->delete();
         session()->flash('message','Product has been deleted Successfully!');
     }
