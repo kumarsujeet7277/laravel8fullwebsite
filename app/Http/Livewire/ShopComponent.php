acacf21c591bc71c2b7ba2d59c600eb9559a902c
sujeet kumar <kumarsujeet5565@gmail.com>
2022-11-07 22:26:30 +0530

    WishlistUsingDatabase

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -77,6 +77,7 @@ class ShopComponent extends Component
         if(Auth::check())
         {
             Cart::instance('cart')->store(Auth::user()->email);
+            Cart::instance('wishlist')->store(Auth::user()->email);
         }
 
         return view('livewire.shop-component', ['products' => $product, 'categories'=>$category])->layout('layouts.base');
